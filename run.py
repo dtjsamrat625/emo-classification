b2014eecfafc1d84c02b97f38579c59d000e767b
tdasari-usf <tdasari@usfca.edu>
2023-05-29 20:31:22 -0700

    update run.py

--- run.py
+++ run.py
@@ -6,25 +6,24 @@ from model import nw
 from train import tokenizer, model
 import torch.nn.functional as F
 
-option = st.sidebar.selectbox("Projects", ("Emo detections", 
+option = st.sidebar.selectbox("Projects", ("Emotion detections", 
 												"Project 2"))
 
 st.header(option)
 
-if option == "Emo detections":
-	text = st.text_input("Enter a text:")
+if option == "Emotion detections":
+	st.write("A classifier to label annoyance or admiration content")
+	text = st.text_input("Enter text to be classified")
 	model.eval()
-
 	device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 	encoded_texts = tokenizer(txts, padding=True, truncation=True, return_tensors='pt').to(device)
 	outputs = model(encoded_texts['input_ids'], attention_mask=encoded_texts['attention_mask'])
 	outs = F.softmax(outputs.logits, dim = 1)
-	predicted_labels = torch.argmax(outs, dim=1)
-	predicted_labels
-	st.write('time now is ')
-	st.write(nw)
-	st.write(text)
-
+	predicted_labels = torch.argmax(outs, dim = 1)
+	#assign label 1 to admiration, 0 to annoyance
+	label_dict = { 0:'annoyance', 1: 'admiration'}
+	st.write(label_dict[predicted_labels.item()])
+	
 if option == "Project 2":
-	st.write('Proj 2')
+	st.write('In progress...')
 	
